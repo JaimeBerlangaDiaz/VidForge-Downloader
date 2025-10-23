1123a5b76ca96e315214fa88484299461154efc0
Jaime Berlanga Diaz <jaimeberlangadiaz@MacBook-Air-de-Jaime.local>
2025-10-23 13:22:09 +0200

    Initial project setup with NetBeans

---
+++ mavenproject3/src/main/java/berlanga/vidforgedownloader/Mavenproject3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package berlanga.vidforgedownloader;
+
+/**
+ *
+ * @author jaimeberlangadiaz
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
